3310cfca276bc0043dbc9ff3427da8c40f088fe2
Jordan Gruenewald <jorg1776@byu.edu>
2018-09-07 14:47:28 -0600

    updated main page

--- nuxt-tutorial/pages/index.vue
+++ nuxt-tutorial/pages/index.vue
@@ -1,11 +1,21 @@
 <template>
-  
+  <div id="title">
+    <h1>NuxtJS Tutorial</h1>
+    <p>Followed the tutorial found <a href="https://www.youtube.com/watch?v=nteDXuqBfn0" target="_blank">here</a></p>
+  </div>
 </template>
 
 <script>
 
 </script>
 
-<style>
-
+<style scoped>
+#title {
+  text-align: center;
+  margin: 20px;
+}
+h1 {
+  font-size: 2em;
+  margin: 10px;
+}
 </style>
